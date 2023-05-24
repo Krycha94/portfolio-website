97fa514144b0224579b1f632930502bb5cd80fa0
Krystian Kowalski <krycha1515@gmail.com>
2023-05-24 11:19:09 +0200

    Create constants file in utils with nav links and use it in Header component to make dynamic links, create sass animation file with pulse animation and use it in resume link in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { navLinks } from "../../utils/constants";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import styles from "./Header.module.scss";
 
@@ -5,14 +6,16 @@ const Header = () => {
 	return (
 		<header className={styles.header}>
 			<nav className={styles.header__nav}>
-				<a href="#" className={styles.header__link}>
-					link-1
-				</a>
-				<a href="#" className={styles.header__link}>
-					link-2
-				</a>
-				<a href="#" className={styles.header__link}>
-					link-3
+				{navLinks.map((link) => (
+					<a href={link.address} key={link.id} className={styles.header__link}>
+						{link.text}
+					</a>
+				))}
+				<a
+					href="#"
+					className={`${styles.header__link} ${styles.header__resume}`}
+				>
+					resume
 				</a>
 			</nav>
 			<button className={styles.header__hamburger}>
---
+++ src/utils/constants.tsx
@@ -0,0 +1,22 @@
+export const navLinks = [
+	{
+		id: 1,
+		address: "#home",
+		text: "home",
+	},
+	{
+		id: 2,
+		address: "#about",
+		text: "about",
+	},
+	{
+		id: 3,
+		address: "#projects",
+		text: "projects",
+	},
+	{
+		id: 4,
+		address: "#contact",
+		text: "contact",
+	},
+];
